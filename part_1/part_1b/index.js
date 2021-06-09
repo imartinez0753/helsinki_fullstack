f175a80e11cbd40bd935b2234c2f13ed7f6995c6
Ian Martinez <imartinez0753@gmail.com>
2021-06-09 06:08:50 -0700

    created javascript reference file

--- part_1/part_1b/index.js
+++ part_1/part_1b/index.js
@@ -1,4 +1,52 @@
-const t = [1, -1, 3]
-const t2 = t.concat(5)
+// concat --------------------------------------------------------------
+// const t = [1, -1, 3]
+// const t2 = t.concat(5)
 
-console.log(t, t2)
+// console.log(t, t2)
+
+// Map method------------------------------------------------------------
+
+// const t = [1,2,3]
+// const m1 = t.map(value => value*2)
+// console.log(m1)
+
+// const m2 = t.map(value => '<li>' + value + '<li>') 
+// console.log (m2)
+
+// const t = [1, 2, 3, 4, 5]
+// const [first, second, ...rest] = t
+
+// console.log(first, second)
+// console.log(rest)
+
+//OBJECTS---------------------------------------------------------------
+
+// const object1 = {
+//     name: 'Arto Hellas',
+//     age: 35,
+//     education: 'PhD',
+//   }
+  
+//   const object2 = {
+//     name: 'Full Stack web application development',
+//     level: 'intermediate studies',
+//     size: 5,
+//   }
+  
+//   const object3 = {
+//     name: {
+//       first: 'Dan',
+//       last: 'Abramov',
+//     },
+//     grades: [2, 3, 5, 3],
+//     department: 'Stanford University',
+//   }
+
+//   console.log(object1.name)         
+// const fieldName = 'age' 
+// console.log(object1[fieldName])   
+
+// object1.address = 'Helsinki'
+// object1['secret number'] = 12341
+// console.log(object1)
+
