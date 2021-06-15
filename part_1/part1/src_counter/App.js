12ef98ccd99ad3bbc0753fd9fec6c0fe7d60a82f
Ian Martinez <imartinez0753@gmail.com>
2021-06-15 05:43:27 -0700

    react state

--- part_1/part1/src_counter/App.js
+++ part_1/part1/src_counter/App.js
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 
 
 
@@ -85,74 +85,33 @@
 // }
 // -----------------------------------------------------------------------------------------------------------
 
-// const Hello = ({ name, age}) => {
-//   console.log(name, age)
-//   console.log(new Date().getFullYear() - age)
-//   const bornYear = () => new Date().getFullYear() - age
-
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>
-//         So you were probably born in {bornYear()}
-//       </p>
-//     </div>
-//   )
-// }
-
-// const App = () => {
-//   const name = 'Ian'
-//   const age = 32
-
-//   return (
-//     <div>
-//       <h1>Greetings</h1>
-//       {/* <Hello name="Maya" age={26 + 10} /> */}
-//       <Hello name={name} age={age} />
-//     </div>
-//   )
-// }
-
-//-------------------------------------------------------------------------
-import React, { useState } from 'react'
-
-//=============================================================
-// const App = () => {
-//   const [ counter, setCounter ] = useState(0)
-
-//   setTimeout(
-//     () => setCounter(counter + 1),
-//     1000
-//   )
-//     console.log('rendering...', counter)
-    
-//   return (
-//     <div>{counter}</div>
-//   )
-// }
-//-----------------------------------------------------------
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-
-  const setToZero = () => setCounter(0)
- 
-  
+const Hello = ({ name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you were probably born in {bornYear()}
+      </p>
     </div>
   )
 }
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      {/* <Hello name="Maya" age={26 + 10} /> */}
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+
 export default App
