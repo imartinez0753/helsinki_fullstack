16b1397f44f83e76b895312bd7c00211a34d769d
Ian Martinez <imartinez0753@gmail.com>
2021-06-17 05:42:56 -0700

    conditional rendering, Handling arrays, complex state

--- part_1/part1/src/App.js
+++ part_1/part1/src/App.js
@@ -168,20 +168,72 @@
 // }
 //================================================================
 import React, { useState } from 'react'
+// const App = () => {
+//   const [clicks, setClicks] = useState({
+//     left: 0, right: 0
+//   })
+
+//   const handleLeftClick = () => {
+//     setClicks({ ...clicks, left: clicks.left + 1})
+//   }
+
+//   const handleRightClick = () => {
+//     setClicks({ ...clicks, right: clicks.right + 1})
+//   }
+
+//   return (
+//     <div>
+//       {clicks.left}
+//       <button onClick={handleLeftClick}>left</button> 
+//       <button onClick={handleRightClick}>right</button>
+//       {clicks.right}
+//     </div>
+//   )
+// }
+//-----------------------------------------------------------------
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  }
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
 
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
