fddad7ae4d396aa923dbb2164636c31a42231f60
Ian Martinez <imartinez0753@gmail.com>
2021-06-23 05:14:49 -0700

    ex 1.6

--- part_1/exercises 1.6.-1.14/unicafe/src/App.js
+++ part_1/exercises 1.6.-1.14/unicafe/src/App.js
@@ -5,10 +5,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+    const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+  const handleGoodClick = () => {
+        setGood(good + 1)
+      }
+  const handleNeutralCLick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadCLick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <h3>give feedback</h3>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralCLick} text='neutral' />
+      <Button handleClick={handleBadCLick} text='bad' />
+      <h3>statistics</h3>
+      {'good: ' + good + ' '}
+      {'neutral: ' + neutral + ' '}
+      {'bad: ' + bad}
     </div>
   )
 }
